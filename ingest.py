51619156693d2071e212e5ba929825c256eafa55
AndreaMoschetto <andreamoschetto99@gmail.com>
2025-11-05 18:59:18 +0100

    chore: update langchain-chroma dependency

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 import os
 import pymupdf4llm as pymu
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
